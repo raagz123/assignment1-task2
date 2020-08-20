7bb36ecab3fec424f4ca6b92bcabc57a40f81390
raagz123 <aluri.venkatraghavender12@gmail.com>
2020-08-20 21:16:23 +1000

    Updating the js file with comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,56 @@
-incdiv = 0
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+   $('#myModal').on('shown.bs.modal', function () {
+   $('#myInput').trigger('focus')
+   })
 
+   //Counter to keep track of new post id
+   incdiv = 0
+   
+   // Function for posting the images on the html page
+   function preview_image(event) {
 
-function preview_image(event) {
-var reader = new FileReader();
-reader.onload = function(){
+   // Uploading the images from the file explorer of device
+   var reader = new FileReader();
+   reader.onload = function(){
 
-	var today = new Date()
+      // fetches the current data
+      var today = new Date()
 
-	console.log(today)
+   // Creating a div for the new post and setting the attributes for new div
+   var div = document.createElement('div');
+         div.style.width = "32rem";
+         div.className = 'card'
 
-  var div = document.createElement('div');
-       div.style.width = "32rem";
-       div.className = 'card'
-       div.id = 'postid'+incdiv
-       document.getElementById('newdiv').insertBefore(div,document.getElementById('newdiv').firstChild);
-document.getElementById('postid'+incdiv).innerHTML +=" <h10 class=' card-content'>\
-                  <img src='images/Logo2.png' height='30' class='round' alt=''> <strong> Saiyajin_Pride </strong>\
-               </h10>\
-               <p class='paid-text card-content' > "+today+"</p>\
-               <img  height='500' id='file"+incdiv+"' class='card-img-top card-image' alt='...'>\
-               <p>\
-                  <a href='#' class=''> <i class='far fa-heart insta-button'></i></a>\
-                  <a href='#' class=''> <i class='far fa-comment insta-button'></i></a>\
-                  <a href='#' class=''> <i class='far fa-paper-plane insta-button'></i></a>\
-               </p>\
-               <p class='card-text'>745 likes</p>\
-               <div class='comment-section comment-bar'>\
-               <form action='.'>\
-                  <div class='input-group'>\
-                     <input type='text' class='form-control comment'placeholder='Add a Comment ...' >\
-                     <div class='input-group-append'>\
-                        <button class='btn post-button ' type='submit'> Post </button>\
+         // Setting new postId
+         div.id = 'postid'+incdiv
+
+         // creating a parent div to insert new post every time as first child
+         parent = document.getElementById('newdiv')
+         document.getElementById('newdiv').insertBefore(div,parent.firstChild);
+   document.getElementById('postid'+incdiv).innerHTML +=" <h10 class=' card-content'>\
+                     <img src='images/Logo2.png' height='30' class='round' alt=''> <strong> Saiyajin_Pride </strong>\
+                  </h10>\
+                  <p class='paid-text card-content' > "+today+"</p>\
+                  <img  height='500' id='file"+incdiv+"' class='card-img-top card-image' alt='...'>\
+                  <p>\
+                     <a href='#' class=''> <i class='far fa-heart insta-button'></i></a>\
+                     <a href='#' class=''> <i class='far fa-comment insta-button'></i></a>\
+                     <a href='#' class=''> <i class='far fa-paper-plane insta-button'></i></a>\
+                  </p>\
+                  <p class='card-text'>745 likes</p>\
+                  <div class='comment-section comment-bar'>\
+                  <form action='.'>\
+                     <div class='input-group'>\
+                        <input type='text' class='form-control comment'placeholder='Add a Comment ...' >\
+                        <div class='input-group-append'>\
+                           <button class='btn post-button ' type='submit'> Post </button>\
+                        </div>\
                      </div>\
-                  </div>\
-               </form>\
-            </div>\
-               ";
-  var output = document.getElementById('file'+incdiv);
-  // console.log(output)
-  output.src = reader.result;
-}
-reader.readAsDataURL(event.target.files[0]);
-incdiv=incdiv+1
-}
+                  </form>\
+               </div>\
+                  ";
+   var output = document.getElementById('file'+incdiv);
+   output.src = reader.result;
+   }
+   reader.readAsDataURL(event.target.files[0]);
+   incdiv=incdiv+1
+   }
--- server.js
+++ server.js
@@ -3,11 +3,6 @@ let app=express()
 
 app.use(express.static(__dirname+ '/public'));
 
-app.get('/',function(request,response){
-    response.send('Hello');
-
-})
-
 // Start the server on port 8000
 app.listen(8000)
  
